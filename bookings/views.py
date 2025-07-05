f147c7e4b6767b7de2dd2d71b6c6ff1c952aaea0
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-07-05 14:47:19 +0100

    feat: added log in required to view bookings

--- bookings/views.py
+++ bookings/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .forms import BookingForm
 from .models import Booking  
+from django.views.decorators.http import require_POST
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def booking_form(request):
@@ -17,12 +19,16 @@ def booking_form(request):
 def booking_success(request):
     return render(request, 'bookings/booking_success.html')
 
-
+@login_required
 def booking_list(request):
-    bookings = Booking.objects.all().order_by('-booking_date', '-booking_time')
+    if request.user.is_staff:
+        bookings = Booking.objects.all().order_by('-booking_date', '-booking_time')
+    else:
+         bookings = Booking.objects.all().order_by('-booking_date', '-booking_time')
     return render(request, 'bookings/booking_list.html', {'bookings': bookings})
 
 
 def booking_detail(request, pk):
     booking = get_object_or_404(Booking, pk=pk)
     return render(request, 'bookings/booking_detail.html', {'booking': booking})
+
