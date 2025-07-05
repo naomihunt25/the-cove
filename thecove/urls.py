6899dcb580eb0a3c4d451ccb1104e1f37c823aef
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-07-05 15:27:39 +0100

    fix url patterns

--- thecove/urls.py
+++ thecove/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('bookings.urls')),
     path('bookings/', include('bookings.urls')),  
     path('accounts/', include('django.contrib.auth.urls')),
 ]
