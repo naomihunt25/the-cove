485780b386d1b3741c234ba06056130421e0ae57
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-07-05 15:41:50 +0100

    fix: removed lines in cove urls to avoid duplicates

--- thecove/urls.py
+++ thecove/urls.py
@@ -20,6 +20,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('bookings.urls')),
-    path('bookings/', include('bookings.urls')),  
-    path('accounts/', include('django.contrib.auth.urls')),
 ]
