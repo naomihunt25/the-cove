601f3aa0980201d9756c5076b7caaa268ca2e2ed
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-07-05 14:35:15 +0100

    feat: Add the ability to create a new booking from the list page

--- bookings/urls.py
+++ bookings/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.booking_list, name='booking_list'),
-    path('new/', views.booking_create, name='booking_create_view'),
+    path('bookings/new/', views.booking_form, name='booking_form'),
     path('success/', views.booking_success_view, name='booking_success'),
     path('<int:pk>/', views.booking_detail, name='booking_detail'),
 ]
