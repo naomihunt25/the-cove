bc8549935a10c075583b6aefa8c5acd71b29054e
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-07-05 15:39:21 +0100

    fix: Added correct path to bookings url

--- bookings/urls.py
+++ bookings/urls.py
@@ -11,6 +11,6 @@ urlpatterns = [
 # Bookings
     path('list/', views.booking_list, name='booking_list'),
     path('bookings/new/', views.booking_form, name='booking_form'),
-    path('success/', views.booking_success, name='booking_success'),
-    path('<int:pk>/', views.booking_detail, name='booking_detail'),
+    path('bookings/success/', views.booking_success, name='booking_success'),
+    path('bookings/<int:pk>/', views.booking_detail, name='booking_detail'),
 ]
