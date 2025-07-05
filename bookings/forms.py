d446ce408f9fbe66193b4adc9f2786e445e60915
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-07-06 00:15:05 +0100

    feat: update forms

--- bookings/forms.py
+++ bookings/forms.py
@@ -54,8 +54,6 @@ class BookingForm(forms.ModelForm):
 
     def clean_booking_time(self):
         booking_time = self.cleaned_data.get('booking_time')
-        if isinstance(booking_time, str):
-            booking_time = datetime.strptime(booking_time, "%H:%M").time()
         valid_times = [choice[0] for choice in self.TIME_CHOICES]
         if booking_time not in valid_times:
             raise forms.ValidationError("Please select a valid booking time.")
