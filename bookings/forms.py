41dcc70d8dbfcc317cd640c58724fd01066849cb
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-07-06 00:24:45 +0100

    fix: amended indents in forms

--- bookings/forms.py
+++ bookings/forms.py
@@ -3,25 +3,25 @@ from datetime import date, time, datetime
 from .models import Booking
 
 
-    TIME_CHOICES = [
-        (time(12, 0), '12:00 PM'),
-        (time(12, 30), '12:30 PM'),
-        (time(13, 0), '1:00 PM'),
-        (time(13, 30), '1:30 PM'),
-        (time(14, 0), '2:00 PM'),
-        (time(14, 30), '2:30 PM'),
-        (time(15, 0), '3:00 PM'),
-        (time(15, 30), '3:30 PM'),
-        (time(16, 0), '4:00 PM'),
-        (time(16, 30), '4:30 PM'),
-        (time(17, 0), '5:00 PM'),
-        (time(17, 30), '5:30 PM'),
-        (time(18, 0), '6:00 PM'),
-        (time(18, 30), '6:30 PM'),
-        (time(19, 0), '7:00 PM'),
-        (time(19, 30), '7:30 PM'),
-        (time(20, 0), '8:00 PM'),
-    ]
+TIME_CHOICES = [
+    (time(12, 0), '12:00 PM'),
+    (time(12, 30), '12:30 PM'),
+    (time(13, 0), '1:00 PM'),
+    (time(13, 30), '1:30 PM'),
+    (time(14, 0), '2:00 PM'),
+    (time(14, 30), '2:30 PM'),
+    (time(15, 0), '3:00 PM'),
+    (time(15, 30), '3:30 PM'),
+    (time(16, 0), '4:00 PM'),
+    (time(16, 30), '4:30 PM'),
+    (time(17, 0), '5:00 PM'),
+    (time(17, 30), '5:30 PM'),
+    (time(18, 0), '6:00 PM'),
+    (time(18, 30), '6:30 PM'),
+    (time(19, 0), '7:00 PM'),
+    (time(19, 30), '7:30 PM'),
+    (time(20, 0), '8:00 PM'),
+]
 
 class BookingForm(forms.ModelForm):
 
