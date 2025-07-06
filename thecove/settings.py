d80b064828c176404a16ea4849e14e7d243d0611
naomihunt25 <7tkz8agg@students.codeinstitute.net>
2025-07-06 11:51:12 +0100

    fix: br tags on about page for validator

--- thecove/settings.py
+++ thecove/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x(+6%h9&%#$n+$72muzgllmziftb)ap+h=mqd&95bsas)8a!iu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '.herokuapp.com']
 
